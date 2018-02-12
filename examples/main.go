df1398e4688ddbb4b4520e2bab50ab81efa3b4b6
wzshiming <wzshiming@foxmail.com>
2018-02-12 16:09:08 +0800

    add examples

---
+++ examples/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"gopkg.in/ffmt.v1"
+
+	checker "gopkg.in/checker.v1"
+)
+
+type TT0 struct {
+	B string `checker:"len 5"`
+	C int    `checker:"range 5 19"`
+}
+
+func main() {
+	checkAll := checker.NewCheckerAll()
+	check := checker.NewChecker()
+	var err error
+	d0 := []struct {
+		A  string `checker:"len 4,regexp d"`
+		Ts []TT0  `checker:"len 2"`
+	}{
+		{
+			A: "ssa",
+		},
+	}
+	err = check.Check(d0)
+	if err != nil {
+		ffmt.Mark(err)
+		ffmt.Puts(d0)
+	}
+	/*
+		main.go:28  .A: failed `len 4` : 3 != 4
+		[
+		 {
+		  A:  "ssa"
+		  Ts: [ ]
+		 }
+		]
+	*/
+
+	err = checkAll.Check(d0)
+	if err != nil {
+		ffmt.Mark(err)
+		ffmt.Puts(d0)
+	}
+	/*
+		main.go:43
+		Checker failed:
+		.A: failed `len 4` : 3 != 4
+		.A: failed `regexp d` : ssa
+		.Ts: failed `len 2` : 0 != 2
+
+		[
+		 {
+		  A:  "ssa"
+		  Ts: [ ]
+		 }
+		]
+	*/
+
+	d1 := struct {
+		A  string `checker:"len 4,regexp ^ss"`
+		Ts []TT0  `checker:"check"`
+	}{
+		A: "aa",
+		Ts: []TT0{
+			{
+				B: "13",
+			},
+		},
+	}
+	err = checkAll.Check(d1)
+	if err != nil {
+		ffmt.Mark(err)
+		ffmt.Puts(d1)
+	}
+	/*
+		main.go:74
+		Checker failed:
+		.A: failed `len 4` : 2 != 4
+		.A: failed `regexp ^ss` : aa
+		.Ts:
+		Checker failed:
+		TT0.B: failed `len 5` : 2 != 5
+		TT0.C: failed `range 5 19` : 0 < 5
+
+		{
+		 A:  "aa"
+		 Ts: [
+		  {
+		   B: "13"
+		   C: 0
+		  }
+		 ]
+		}
+	*/
+}
